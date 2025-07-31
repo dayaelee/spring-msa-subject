d3fc9eff46d6d8a4b6e511a5579f4c4c1ef43e3e
dayaelee <dayae.dev@gmail.com>
2025-07-31 22:07:58 +0900

    feat: JPA Auditing 활성화를 위한 @EnableJpaAuditing 추가

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/ProductApplication.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/ProductApplication.java
@@ -3,9 +3,11 @@ package com.sparta.msa_exam.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableJpaAuditing
 public class ProductApplication {
 
 	public static void main(String[] args) {
