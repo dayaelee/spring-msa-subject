8b55436f88f92bc2a71ddea95b4c3ffa40f4288e
dayaelee <dayae.dev@gmail.com>
2025-08-02 01:56:37 +0900

    feat: 주문 생성, 상품 추가, 조회 기능 컨트롤러 구현

---
+++ com.sparta.msa_exam.order/src/main/java/com/sparta/msa_exam/order/controller/OrderController.java
@@ -0,0 +1,64 @@
+package com.sparta.msa_exam.order.controller;
+
+import com.sparta.msa_exam.order.dto.OrderProductRequestDto;
+import com.sparta.msa_exam.order.dto.OrderProductResponseDto;
+import com.sparta.msa_exam.order.dto.OrderRequestDto;
+import com.sparta.msa_exam.order.dto.OrderResponseDto;
+import com.sparta.msa_exam.order.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+// 이 클래스는 REST API 엔드포인트다.
+// 메서드가 리턴하는 값은 바로 HTTP 응답 본문(Body)에 넣겠다.
+@RequiredArgsConstructor
+// Lombok에서 제공하는 어노테이션
+// final이 붙은 필드나 @NonNull이 붙은 필드만 골라서 자동으로
+// 생성자를 만들어줌
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/orders")
+    public ResponseEntity<OrderResponseDto> addOrder(
+            @RequestBody OrderRequestDto order,
+            @RequestParam(value = "fail", required = false) boolean fail) {
+        // @ModelAttribute 형태는 지금 상황에서 부적절함
+        //    {
+        //        "products": [
+        //        { "product_id": 99 },
+        //        { "product_id": 88 }
+        //        ]
+        //    }
+        // 이런 식으로 하나의 주문에 (상품 여러개) 받을 것이기 때문임 => JSON 요청
+
+        // 주문 추가 쿼리 호출
+        OrderResponseDto orderResponseDto = orderService.save(order, fail);
+
+        return ResponseEntity.ok(orderResponseDto);
+    }
+
+    // 주문에 상품을 추가하는 API
+    @PutMapping("/orders/{orderId}")
+    public ResponseEntity<OrderResponseDto> addProduct(@PathVariable("orderId") Long order_id, @RequestBody OrderProductRequestDto product){
+        OrderResponseDto orderResponseDto = orderService.add(order_id, product.getProduct_id());
+
+        return ResponseEntity.ok(orderResponseDto);
+    }
+
+    // 주문 단 건 조회 API
+    @GetMapping("/orders/{orderId}")
+    public ResponseEntity<OrderResponseDto> getOrder(@PathVariable("orderId") Long order_id){
+//        OrderProductResponseDto orderProductResponseDto = orderService.get(order_id);
+        OrderResponseDto orderResponseDto = orderService.get(order_id);
+        return ResponseEntity.ok(orderResponseDto);
+    }
+
+
+
+
+
+
+
+}
