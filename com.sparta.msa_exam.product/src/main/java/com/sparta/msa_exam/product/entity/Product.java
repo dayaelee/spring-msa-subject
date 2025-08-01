83f5ab8c7b2418778d675a38beb41aa20f0e7295
dayaelee <dayae.dev@gmail.com>
2025-08-02 01:50:28 +0900

    refactor: 사용하지 않는 import 삭제

--- com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/entity/Product.java
+++ com.sparta.msa_exam.product/src/main/java/com/sparta/msa_exam/product/entity/Product.java
@@ -1,12 +1,10 @@
 package com.sparta.msa_exam.product.entity;
 
 
-import com.sparta.msa_exam.product.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 
 @Getter
 @Entity
